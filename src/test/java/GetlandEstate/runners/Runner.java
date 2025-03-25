cc743eb80fec9da8005ce03c9082013cbf330950
Fatma Yasar <fatmasemizyasar2023@gmail.com>
2025-03-25 16:20:10 +0100

    APIUS6-12-13Yapildi

--- src/test/java/GetlandEstate/runners/Runner.java
+++ src/test/java/GetlandEstate/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         glue = {"GetlandEstate/stepdefs","GetlandEstate/hooks"},
 
 
-        tags = "@apiAdmin ",
+        tags = "@US-06",
 
 
         dryRun = true
